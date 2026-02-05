c106a51d768ccde998e70f89ca2fdd44606d5da0
Marina <esakova.marina@gmail.com>
2026-02-04 19:58:31 -0800

    Day 8

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,13 @@
+def greet():
+    print("Hello Marina")
+    print("How are you Marina?")
+    print("Good job learning Python Marina")
+
+greet()
+
+def greet_with_name(name):
+    print(f"Hello, {name}!")
+    print(f"How are you {name}?")
+    print(f"Good bye, {name}!")
+
+greet_with_name("Marina")
